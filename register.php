636b4e533f1c998e0bd9b61d82e052e5e8158553
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2020-12-04 15:10:11 +0100

    changed id names in MySQL database

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username_err = "Please enter a username.";
     } else{
         // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
+        $sql = "SELECT user_id FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -113,6 +113,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 header("location: login.php");
             } else{
                 echo "Something went wrong. Please try again later.";
+                echo "ERROR:\n" ;
+                echo mysqli_stmt_errno($stmt);
+                echo "ERROR:\n" ;
+                echo mysqli_stmt_error($stmt);
             }
 
             // Close statement
