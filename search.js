59f039fd9d8a364b7699c865028d681aca770bc6
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2021-01-16 22:23:27 +0100

    better code for simple search

--- search.js
+++ search.js
@@ -1,22 +1,25 @@
+"use strict";
+
 function simpleSearch() {
     //parse every row in tables and displays only the ones that match (partially) the search
     //both the search term and the cell content are set to lowercase and accents are removed to improve ease of search
-
     let rows = document.getElementsByTagName('tr');
-    let searchString = document.getElementById('search-bar').value.toLowerCase().normalize('NFKD').replace(/[^\w]/g, '');
-    for (let i_row = 1; i_row < rows.length; i_row++) {
-        let cell = rows[i_row].getElementsByTagName('td');
-        let present = 0;
-        for (let i_col = 0; i_col < cell.length - 2; i_col++) {
-            let cell_content = cell[i_col].innerHTML.toLowerCase().normalize('NFKD').replace(/[^\w]/g, '');
-            if (cell_content.indexOf(searchString) > -1) {
-                present = 1;
-            }
+    for (let row of rows) {
+        const cells = row.getElementsByTagName('td');
+        let present = false;
+        for (const cell of cells) {
+            present = present || matchSearch(cell.innerHTML);
         }
-        if (present == 1) {
-            rows[i_row].style.display = '';
-        } else {
-            rows[i_row].style.display = 'none';
-        }
+        row.style.display = present ? '' : 'none';
     }
+}
+
+function matchSearch(content) {
+    let searchString = cleanString(document.getElementById('search-bar').value);
+    let content_clean = cleanString(content);
+    return content_clean.indexOf(searchString) > -1;
+}
+
+function cleanString(dirtyString) {
+    return dirtyString.toLowerCase().normalize('NFKD').replace(/[^\w]/g, '');
 }
