d92aeb740f6c31d7ed5b600a4d0de609f8e7b206
Emilion Goddet <e.goddet58@gmail.com>
2021-01-25 12:44:06 +0100

    navbar on top and responsive

--- index.php
+++ index.php
@@ -57,220 +57,209 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 	</script>
 
 	<style>
-		.student-name span {
+		.list-name span {
 			display: block;
 			text-align: left;
 		}
 
-		.student-meta span {
+		.list-meta span {
 			display: block;
 			text-align: right;
 		}
 
-		.table-mobility tr td {
+		.list tr td {
 			padding: 8px 0;
 			border-bottom: 1px solid #ddd;
 		}
 
-		.table-mobility {
+		.list {
 			width: 100%;
+		}
+
+		.list-edit {
+			text-align: right;
 		}
 	</style>
 </head>
 
 <body>
 	<div class="wrap-all">
-		<div class="barleft">
-			<div class="user-info">
+		<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+			<a class="navbar-brand" href="#">Students Mobility in TSE</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					<button class="btn btn-info" onclick="viewMap()">View Map <span class="sr-only">(current)</span></button>
+					<button class="btn btn-info" onclick="window.location.href='mobility.php'">New Mobility</button>
+					<button class="btn btn-info" onclick="window.location.href='partner.php'">New Partner</button>
+					<button class="btn btn-info" onclick="window.location.href='logout.php'">Logout</button>
+							<?php
+							if ($_SESSION['is_admin']) {
+								echo "<button type=\"button\" class=\"btn btn-info\" onclick=\"window.location.href='user.php'\">New User</button>";
+							}
+							?>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
+	</div>
+	<div class="bartop">
+		<h1>Student Mobilities in Telecom Saint Etienne</h1>
+	</div>
+	<div class="main">
+		<input type="search" id="search-bar" placeholder="filter results" onkeyup="simpleSearch()" size="16">
+		<div class="container-lg">
+			<div id="table" style="display: block">
+				<div>
+					<table class="list list-mobility">
+						<h1>Mobilities</h1>
+						<tbody class="mobility-tbody">
+							<?php
+							//query
+							$query_mobilities = "SELECT * FROM mobilities JOIN users USING(user_id) JOIN partners USING(partner_id);";
+							$result_mobilities =  mysqli_query($link, $query_mobilities);
+							//$num = mysqli_num_rows($result_mobilities);//number of rows from the query
+
+							//display in table
+							while ($row = mysqli_fetch_array($result_mobilities)) {
+								echo "<tr class=\"list-mobility\">";
+								echo "<td class=\"list-name\">";
+								echo "<span><strong>" . $row['firstname'] . " " . $row['lastname'] . "</strong></span>";
+								echo "<span>" . $row['promotion'] .  "</span></td>";
+								echo "<td class=\"list-meta\">";
+								echo "<span>" . $row['country'] . ", " . $row['city'] . "</span>";
+								echo "<span>" . $row['date_start'] . " / " . $row['date_stop'] . "</span></td>";
+								if ($_SESSION["is_admin"]) {
+									echo "<td class=\"list-edit\">";
+									$temp = $row['mobility_id'];
+									echo "<a data-toggle=\"tooltip\" href=\"/mobility.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
+									echo "</td>";
+								};
+								echo "</tr>";
+							};
+							?>
+						</tbody>
+					</table>
+					<table class="list list-users">
+						<h1>Users</h1>
+						<tbody>
+							<!-- get users from Database -->
+							<?php
+							//query
+							$query_users = "SELECT user_id, firstname, lastname, promotion, email, comment, admin, created_at FROM users;";
+							$result_users =  mysqli_query($link, $query_users);
+
+							//display in table
+							while ($row = mysqli_fetch_array($result_users)) {
+								echo "<tr class=\"list-users\">";
+								echo "<td class=\"list-name\">";
+								$isAdmin = "";
+								if ($row['admin'] == '1')
+									$isAdmin = "ADMIN";
+								echo "<span><strong>" . $row['firstname'] . " " . $row['lastname'] . "</strong><strong class=\"admin\"
+									style=\"color: red\"> " . $isAdmin . "</strong></span>";
+								echo "<span>" . $row['promotion'] . "</span></td>";
+								echo "<td class=\"list-comment\">" . $row['comment'] . "</td>";
+								echo "<td class=\"list-meta\">";
+								echo "<span" . $row['email'] . "</span>";
+								echo "<span>" . $row['created_at'] . "</span></td>";
+								if ($_SESSION["is_admin"]) {
+									echo "<td class=\"list-edit\">";
+									$temp = $row['mobility_id'];
+									echo "<a data-toggle=\"tooltip\" href=\"/mobility.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
+									echo "</td>";
+								};
+								echo "</tr>";
+							};
+							?>
+						</tbody>
+					</table>
+					<table class="list list-partners">
+						<h1>Partners</h1>
+						<tbody>
+							<!-- get partners from Database -->
+							<?php
+							//query
+							$query_partners = "SELECT * FROM partners;";
+							$result_partners =  mysqli_query($link, $query_partners);
+
+							//display in table
+							while ($row = mysqli_fetch_array($result_partners)) {
+								echo "<tr class=\"list-partners\">";
+								echo "<td class=\"list-name\">";
+								echo "<span><strong>" . $row['name'] . "</span></strong>";
+								echo "<span>" . $row['country'] . ", " . $row['city'] . "</span></td>";
+								echo "<td class=\"list-meta\">";
+								echo "<span>" . $row['location1'] . "</span>";
+								echo "<span>" . $row['location2'] . "</span></td>";
+								if ($_SESSION["is_admin"]) {
+									echo "<td class=\"list-edit\">";
+									$temp = $row['mobility_id'];
+									echo "<a data-toggle=\"tooltip\" href=\"/mobility.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
+									echo "</td>";
+								};
+								echo "</tr>";
+							};
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="div-map" id="map" style="display: none">
+			<div id="mapID">
+				<!--map is here-->
+			</div>
+			<script type='text/javascript'>
 				<?php
-				echo $_SESSION["username"];
-				echo $_SESSION["is_admin"] ? " (admin) " : "";
+				//this code passes the result of the mysql request in php to a javascript array
+				$php_array = [];
+				$query_mobilities = "SELECT location1, location2, name, firstname, lastname, partner_id, city, country FROM mobilities JOIN users USING(user_id) JOIN partners USING(partner_id);";
+				$result_mobilities =  mysqli_query($link, $query_mobilities);
+				while ($row = mysqli_fetch_array($result_mobilities)) {
+					$loc = [
+						$row['name'], //0
+						floatval($row['location1']), //1
+						floatval($row['location2']), //2
+						$row['firstname'], //3
+						$row['lastname'], //4
+						$row['partner_id'], //5
+						$row['city'], //6
+						$row['country'] //7
+					];
+					array_push($php_array, $loc);
+				};
+				$js_array = json_encode($php_array);
+				echo "var mobilities = " . $js_array . ";\n";
 				?>
-			</div>
-			<button type="button" class="btn btn-info" onclick="window.location.href='logout.php'">Logout</button>
-			<button type="button" class="btn btn-info view-map" id="btn-map" onclick="viewMap()">View Map</button>
-			<button type="button" class="btn btn-info" onclick="window.location.href='mobility.php'">New Mobility</button>
-			<button type="button" class="btn btn-info" onclick="window.location.href='partner.php'">New Partner</button>
-			<?php
-			if ($_SESSION['is_admin']) {
-				echo "<button type=\"button\" class=\"btn btn-info\" onclick=\"window.location.href='user.php'\">New User</button>";
-			}
-			?>
+				var mymap = L.map('mapID').setView([20, 0], 3);
+				var map_markers = new MapMarkers(mobilities);
+
+				L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+					maxZoom: 18,
+					attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+						'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+						'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+					id: 'mapbox/streets-v11',
+					tileSize: 512,
+					zoomOffset: -1
+				}).addTo(mymap);
+
+				map_markers.partners.forEach(function(p) {
+					marker = new L.marker(p.getLngLat())
+						.bindPopup(p.getPopupText())
+						.addTo(mymap);
+				});
+			</script>
 		</div>
-		<div class="bartop">
-			<h1>Student Mobilities in Telecom Saint Etienne</h1>
-		</div>
-		<div class="main">
-			<input type="search" id="search-bar" placeholder="filter results" onkeyup="simpleSearch()" size="16">
-			<div class="container-lg">
-				<div class="list-group" id="table" style="display: block">
-					<div>
-						<table class="table-mobility">
-							<h1>Mobilities</h1>
-							<tbody class="mobility-tbody">
-								<?php
-								//query
-								$query_mobilities = "SELECT * FROM mobilities JOIN users USING(user_id) JOIN partners USING(partner_id);";
-								$result_mobilities =  mysqli_query($link, $query_mobilities);
-								//$num = mysqli_num_rows($result_mobilities);//number of rows from the query
 
-								//display in table
-								while ($row = mysqli_fetch_array($result_mobilities)) {
-									echo "<tr class=\"student-mobility\">";
-									echo "<td class=\"student-name\">";
-									echo "<span><strong>" . $row['firstname'] . " " . $row['lastname'] . "</strong></span>";
-									echo "<span class=\"student-promo\">" . $row['promotion'] . " (" .  $row['username'] .")" . "</span></td>";
-									echo "<td class=\"student-meta\">";
-									echo "<span class=\"student-country\">" . $row['country'] . ", " . $row['city'] . "</span>";
-									echo "<span class=\"student-dates\">" . $row['date_start'] . " / " . $row['date_stop'] . "</span></td>";
-									if ($_SESSION["is_admin"]) {
-										echo "<td class=\"student-edit\">";
-										$temp = $row['mobility_id'];
-										echo "<a data-toggle=\"tooltip\" href=\"/mobility.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
-										echo "</td>";
-									};
-									echo "</tr>";
-								};
-								?>
-							</tbody>
-						</table>
-						<table class="table table-bordered" id="table-users">
-							<h1>Users</h1>
-							<thead>
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Promotion</th>
-									<th>comment</th>
-									<th>admin</th>
-									<th>created</th>
-									<th>email</th>
-									<?php if ($_SESSION["is_admin"]) {
-										echo "<th>Edit</th>";
-									} ?>
-								</tr>
-							</thead>
-							<tbody>
-								<!-- get users from Database -->
-								<?php
-								//query
-								$query_users = "SELECT user_id, firstname, lastname, promotion, email, comment, admin, created_at FROM users;";
-								$result_users =  mysqli_query($link, $query_users);
-
-								//display in table
-								while ($row = mysqli_fetch_array($result_users)) {
-									echo "<tr>";
-									echo "<td>" . $row['firstname'] . "</td>";
-									echo "<td>" . $row['lastname'] . "</td>";
-									echo "<td>" . $row['promotion'] . "</td>";
-									echo "<td>" . $row['comment'] . "</td>";
-									echo "<td>" . $row['admin'] . "</td>";
-									echo "<td>" . $row['created_at'] . "</td>";
-									echo "<td>" . $row['email'] . "</td>";
-									if ($_SESSION["is_admin"]) {
-										echo "<td>";
-										$temp = $row['user_id'];
-										echo "<a class=\"edit\" title=\"Edit\" data-toggle=\"tooltip\" href=\"/user.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
-										//echo "<a class=\"delete\" title=\"Delete\" data-toggle=\"tooltip\"><i class=\"material-icons\">&#xE872;</i></a>";
-										echo "</td>";
-									}
-									echo "</tr>";
-								};
-								?>
-							</tbody>
-						</table>
-						<table class="table table-bordered" id="table-partners">
-							<h1>Partners</h1>
-							<thead>
-								<tr>
-									<th>Name</th>
-									<th>Country</th>
-									<th>City</th>
-									<th>location X</th>
-									<th>location Y</th>
-									<?php if ($_SESSION["is_admin"]) {
-										echo "<th>Edit</th>";
-									} ?>
-								</tr>
-							</thead>
-							<tbody>
-								<!-- get partners from Database -->
-								<?php
-								//query
-								$query_partners = "SELECT * FROM partners;";
-								$result_partners =  mysqli_query($link, $query_partners);
-
-								//display in table
-								while ($row = mysqli_fetch_array($result_partners)) {
-									echo "<tr>";
-									echo "<td>" . $row['name'] . "</td>";
-									echo "<td>" . $row['country'] . "</td>";
-									echo "<td>" . $row['city'] . "</td>";
-									echo "<td>" . $row['location1'] . "</td>";
-									echo "<td>" . $row['location2'] . "</td>";
-									if ($_SESSION["is_admin"]) {
-										echo "<td>";
-										$temp = $row['partner_id'];
-										echo "<a class=\"edit\" title=\"Edit\" data-toggle=\"tooltip\" href=\"/partner.php?id_edit=$temp\"><i class=\"material-icons\">&#xE254;</i></a>";
-										//echo "<a class=\"delete\" title=\"Delete\" data-toggle=\"tooltip\"><i class=\"material-icons\">&#xE872;</i></a>";
-										echo "</td>";
-									}
-									echo "</tr>";
-								};
-								?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-			<div class="div-map" id="map" style="display: none">
-				<div id="mapID">
-					<!--map is here-->
-				</div>
-				<script type='text/javascript'>
-					<?php
-					//this code passes the result of the mysql request in php to a javascript array
-					$php_array = [];
-					$query_mobilities = "SELECT location1, location2, name, firstname, lastname, partner_id, city, country FROM mobilities JOIN users USING(user_id) JOIN partners USING(partner_id);";
-					$result_mobilities =  mysqli_query($link, $query_mobilities);
-					while ($row = mysqli_fetch_array($result_mobilities)) {
-						$loc = [
-							$row['name'], //0
-							floatval($row['location1']), //1
-							floatval($row['location2']), //2
-							$row['firstname'], //3
-							$row['lastname'], //4
-							$row['partner_id'], //5
-							$row['city'], //6
-							$row['country'] //7
-						];
-						array_push($php_array, $loc);
-					};
-					$js_array = json_encode($php_array);
-					echo "var mobilities = " . $js_array . ";\n";
-					?>
-					var mymap = L.map('mapID').setView([20, 0], 3);
-					var map_markers = new MapMarkers(mobilities);
-
-					L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-						maxZoom: 18,
-						attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-							'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-							'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-						id: 'mapbox/streets-v11',
-						tileSize: 512,
-						zoomOffset: -1
-					}).addTo(mymap);
-
-					map_markers.partners.forEach(function(p) {
-						marker = new L.marker(p.getLngLat())
-							.bindPopup(p.getPopupText())
-							.addTo(mymap);
-					});
-				</script>
-			</div>
-
-		</div>
+	</div>
 
 	</div>
 	<?php mysqli_close($link); ?>
